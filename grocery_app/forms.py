79c5992b8f63a5183674d4585e58fdfd3cd4ce2c
tsamantanis <philip.tsamantanis@icloud.com>
2021-04-27 04:19:29 -0700

    Complete forms.py TODOs

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -1,25 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SelectField, SubmitField
+from wtforms import (
+    StringField,
+    DateField,
+    SelectField,
+    SubmitField,
+    FloatField,
+)
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, Length, URL
+from wtforms.validators import DataRequired, Length, URL, NumberRange
+from grocery_app.models import GroceryStore
+
 
 class GroceryStoreForm(FlaskForm):
     """Form for adding/updating a GroceryStore."""
+    title = StringField("Title", validators=[DataRequired(), Length(min = 2, max = 80)])
+    address = StringField("Address", validators=[DataRequired(), Length(min = 2, max = 80)])
+    submit = SubmitField("Submit")
 
-    # TODO: Add the following fields to the form class:
-    # - title - StringField
-    # - address - StringField
-    # - submit button
-    pass
 
 class GroceryItemForm(FlaskForm):
     """Form for adding/updating a GroceryItem."""
 
-    # TODO: Add the following fields to the form class:
-    # - name - StringField
-    # - price - FloatField
-    # - category - SelectField (specify the 'choices' param)
-    # - photo_url - StringField (use a URL validator)
-    # - store - QuerySelectField (specify the `query_factory` param)
-    # - submit button
-    pass
+    name = StringField("Name", validators=[DataRequired(), Length(min = 2, max = 80)])
+    price = FloatField("Price", validators=[DataRequired(), NumberRange(min = 0)])
+    category = SelectField("Category", choices=["PRODUCE", "DELI", "BAKERY", "PANTRY", "FROZEN", "OTHER"], validators = [DataRequired()])
+    photo_url = StringField("Photo Url", validators=[DataRequired(), URL()])
+    store = QuerySelectField("Store", query_factory = lambda: GroceryStore.query, allow_blank = False)
+    submit = SubmitField("Submit")
