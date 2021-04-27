33bce6385f00c12569a3cc18e13b459f90835479
tsamantanis <philip.tsamantanis@icloud.com>
2021-04-27 04:42:03 -0700

    Create new item route & form

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -30,21 +30,27 @@ def new_store():
         )
         db.session.add(new_store)
         db.session.commit()
-        flash("New store was added successfully.")
+        flash("New store added successfully.")
         return redirect(url_for("main.store_detail", store_id = new_store.id))
     return render_template("new_store.html", form = form)
 
 @main.route('/new_item', methods=['GET', 'POST'])
 def new_item():
-    # TODO: Create a GroceryItemForm
-
-    # TODO: If form was submitted and was valid:
-    # - create a new GroceryItem object and save it to the database,
-    # - flash a success message, and
-    # - redirect the user to the item detail page.
-
-    # TODO: Send the form to the template and use it to render the form fields
-    return render_template('new_item.html')
+    """Create new grocery item"""
+    form = GroceryItemForm()
+    if form.validate_on_submit():
+        new_item = GroceryItem(
+            name = form.name.data,
+            price = form.price.data,
+            category = form.category.data,
+            photo_url = form.photo_url.data,
+            store_id = form.store.data.id
+        )
+        db.session.add(new_item)
+        db.session.commit()
+        flash("New item added successfully.")
+        return redirect(url_for("main.item_detail", item_id = new_item.id))
+    return render_template("new_item.html", form = form)
 
 @main.route('/store/<store_id>', methods=['GET', 'POST'])
 def store_detail(store_id):
